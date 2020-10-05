c83744e60657066ea0a685cd718a352f3401a19d
Rishabh <34177647+Rishabh2801@users.noreply.github.com>
2020-10-05 11:50:58 +0530

    Simple Hello World Program
    you will learn how to print statements

---
+++ JavaPrint.java
@@ -0,0 +1,5 @@
+import java.io.*; //imported input-output package
+
+public static void main(String args[]){ //main method(entry point of the program)
+  System.out.println("Hello World"); //print statement or output statement
+}
