5add193947f635d0be0c9be5976e0e6c06e50333
Rishabh <34177647+Rishabh2801@users.noreply.github.com>
2020-10-05 12:14:34 +0530

    Delete JavaAddSub.java

--- JavaAddSub.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public static void main(String args[]){
-
-  int i = 20, j = 30; //declared two integers i and j with value 20 and 30 respectively
-  int sum = i+j; // declared another integer to store the sum of i and j
-  System.out.println(i+" + "+j+" = "+sum); //output will be "20 + 30 = 50"
-
-}
